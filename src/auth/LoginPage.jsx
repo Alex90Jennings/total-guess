c70eb4aaa6fd89bdf00266bc80805177b07071ad
Alex Jennings <aj0312@my.bristol.ac.uk>
2023-02-24 11:17:34 +0100

    basic sign in page

--- src/auth/LoginPage.jsx
+++ src/auth/LoginPage.jsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './loginPage.css'
 
 function LoginPage({ setIsAuthenticated }) {
-    const handleLogin = () => {
-        setIsAuthenticated(true)
-    }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    return (
-        <div>
-            <h1>Login Page</h1>
-            <button onClick={handleLogin}>Login</button>
-        </div>
-    );
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    // IN FUTURE THIS WILL BE THE API CALL FOR THE JWT TOKEN
+    const jwtToken = password
+    console.log(email, password);
+    localStorage.setItem('userEmail', email);
+    localStorage.setItem('jwtToken', jwtToken);
+    setIsAuthenticated(true);
+  };
+
+  return (
+    <div className='form-container'>
+      <form className='form' onSubmit={handleLogin}>
+        <h1>CANTAB</h1>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} required />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} required />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
