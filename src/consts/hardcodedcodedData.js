7602ce092a842751c75f6e38f5353409f9d289f0
Alex Jennings <aj0312@my.bristol.ac.uk>
2023-03-03 09:32:40 +0100

    added description to hard coded data

--- src/consts/hardcodedcodedData.js
+++ src/consts/hardcodedcodedData.js
@@ -1,21 +1,22 @@
 class Grocery {
-    constructor(id, name, shop, price) {
+    constructor(id, name, shop, price, description) {
         this.id = id;
         this.name = name;
         this.shop = shop;
         this.price = price;
+        this.description = description;
     }
 }
 
 export const groceries = [
-    new Grocery("1", "Milk", "Tesco", 1.50),
-    new Grocery("2", "Bread", "Sainsbury's", 1.20),
-    new Grocery("3", "Eggs", "Asda", 2.00),
-    new Grocery("4", "Cheese", "Morrisons", 2.50),
-    new Grocery("5", "Butter", "Waitrose", 1.80),
-    new Grocery("6", "Chicken", "Aldi", 4.00),
-    new Grocery("7", "Apples", "Lidl", 0.80),
-    new Grocery("8", "Carrots", "Co-op", 1.00),
-    new Grocery("9", "Bananas", "Iceland", 1.20),
-    new Grocery("10", "Yogurt", "M&S", 1.50)
+    new Grocery("1", "Milk", "Tesco", 1.50, "milk is good"),
+    new Grocery("2", "Bread", "Sainsbury's", 1.20, "bread is carbs"),
+    new Grocery("3", "Eggs", "Asda", 2.00, "eggs smell funny"),
+    new Grocery("4", "Cheese", "Morrisons", 2.50, "cheese also smells"),
+    new Grocery("5", "Butter", "Waitrose", 1.80, "butter is sexy"),
+    new Grocery("6", "Chicken", "Aldi", 4.00, "chicken is not sexy"),
+    new Grocery("7", "Apples", "Lidl", 0.80, "apples grow on trees"),
+    new Grocery("8", "Carrots", "Co-op", 1.00, "carrots grow underground"),
+    new Grocery("9", "Bananas", "Iceland", 1.20, "bananas look like penis"),
+    new Grocery("10", "Yogurt", "M&S", 1.50, "yogurt is yogurt")
 ];
