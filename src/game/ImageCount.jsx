b1c3d91369a618a38f2caa2c740ea31b5fa45287
Chris Hyde <c_hyde333@hotmail.com>
2023-03-03 12:46:13 +0000

    Changing size of Numbers images

--- src/game/ImageCount.jsx
+++ src/game/ImageCount.jsx
@@ -5,7 +5,7 @@ import NumberImage from './NumberImage';
 function ImageCount(props) {
   const { currentShopIndex } = props;
   return (
-    <div className="timer">
+    <div className="image-counter">
       <NumberImage number={currentShopIndex + 1} />
     </div>
   );
