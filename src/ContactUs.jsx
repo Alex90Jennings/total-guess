d120c1ef4689650004c323cbe520170f77daddb8
Alex Jennings <97463171+Alex90Jennings@users.noreply.github.com>
2023-03-10 18:21:32 +0100

    seperated contact us - now excluded from 100vh

---
+++ src/ContactUs.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function ContactUs() {
+    return (
+        <div className='three-columns-expand-one-three footer--bottom'>
+            <div></div>
+            <div className="footer--list">
+                <div className="contact">Contact</div>
+                <div className="footer-row">About Us</div>
+                <div className="footer-row">FAQ</div>
+                <div className="footer-row">Advertise with us</div>
+            </div>
+            <div className="footer-icon">
+                <img src="/icons/basket.png" alt='basket'/>
+            </div>              
+        </div>
+    );
+}
+
+export default ContactUs;
