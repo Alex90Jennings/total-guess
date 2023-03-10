074b30c769f7724fa08e3dc2d9e258c1f3360327
Alex Jennings <97463171+Alex90Jennings@users.noreply.github.com>
2023-03-10 18:42:23 +0100

    merge

--- src/game/Footer.jsx
+++ src/game/Footer.jsx
@@ -32,18 +32,6 @@ function Footer() {
                 </div>
                 <div></div>
             </div>
-            <div className='three-columns-expand-one-three footer--bottom'>
-                <div></div>
-                <div className="footer--list">
-                    <div className="contact">Contact</div>
-                    <div className="footer-row">About Us</div>
-                    <div className="footer-row">FAQ</div>
-                    <div className="footer-row">Advertise with us</div>
-                </div>
-                <div className="footer-icon">
-                    <img src="/icons/basket.png" alt='basket'/>
-                </div>              
-            </div>
         </footer>
     );
 }
