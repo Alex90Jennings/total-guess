48e55908d14e681249e5eab3f14818e258e4e2b1
Chris Hyde <c_hyde333@hotmail.com>
2023-03-13 12:06:50 +0000

    Swiping action

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-import './styles/App.css';
-import './styles/modal.css';
-import LandingPage from './auth/LandingPage.jsx'
-import MainGamePage from './game/MainGamePage.jsx'
-import GameInstructions from './game/GameInstructions';
-import Modal from 'react-modal';
-import { useState } from 'react';
-import Header from './game/Header';
-import Footer from './game/Footer';
+import "./styles/App.css";
+import "./styles/modal.css";
+import LandingPage from "./auth/LandingPage.jsx";
+import MainGamePage from "./game/MainGamePage.jsx";
+import GameInstructions from "./game/GameInstructions";
+import Modal from "react-modal";
+import { useState } from "react";
+import Header from "./game/Header";
+import Footer from "./game/Footer";
 
 function App() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
-    const [showModal, setShowModal] = useState(false)
-    const [startGame, setStartGame] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [startGame, setStartGame] = useState(false);
 
-    const handleCloseModal = () => setShowModal(false);
+  const handleCloseModal = () => setShowModal(false);
 
-    return (
-        <div className='app'>
-            <Header />
-            {
-                startGame && isAuthenticated ? (
-                    <MainGamePage />
-                ) : (
-                    <LandingPage isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setShowModal={setShowModal} setStartGame={setStartGame} />
-                )
-            }
-            {
-                showModal && (
-                    <Modal className={'modal'} isOpen={showModal} onRequestClose={handleCloseModal}>
-                        <GameInstructions onClose={handleCloseModal}/>
-                    </Modal>
-                )
-            }  
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="app">
+      <Header />
+      {startGame && isAuthenticated ? (
+        <MainGamePage />
+      ) : (
+        <LandingPage
+          isAuthenticated={isAuthenticated}
+          setIsAuthenticated={setIsAuthenticated}
+          setShowModal={setShowModal}
+          setStartGame={setStartGame}
+        />
+      )}
+      {showModal && (
+        <Modal
+          className={"modal"}
+          isOpen={showModal}
+          onRequestClose={handleCloseModal}
+        >
+          <GameInstructions onClose={handleCloseModal} />
+        </Modal>
+      )}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
