175d76b09d53e377cb1fc104498419f4b377af45
Chris Hyde <c_hyde333@hotmail.com>
2023-03-17 09:21:58 +0000

    Adding App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,6 @@ import Modal from "react-modal";
 import { useState } from "react";
 import Header from "./game/Header";
 import Footer from "./game/Footer";
-import "./styles/App.css";
-import "./styles/modal.css";
-import LandingPage from "./auth/LandingPage.jsx";
-import MainGamePage from "./game/MainGamePage.jsx";
-import GameInstructions from "./game/GameInstructions";
-import Modal from "react-modal";
-import { useState } from "react";
-import Header from "./game/Header";
-import Footer from "./game/Footer";
 import ContactUs from "./ContactUs";
 
 function App() {
@@ -25,31 +16,6 @@ function App() {
 
   const handleCloseModal = () => setShowModal(false);
 
-  return (
-    <div className="app">
-      <Header />
-      {startGame && isAuthenticated ? (
-        <MainGamePage />
-      ) : (
-        <LandingPage
-          isAuthenticated={isAuthenticated}
-          setIsAuthenticated={setIsAuthenticated}
-          setShowModal={setShowModal}
-          setStartGame={setStartGame}
-        />
-      )}
-      {showModal && (
-        <Modal
-          className={"modal"}
-          isOpen={showModal}
-          onRequestClose={handleCloseModal}
-        >
-          <GameInstructions onClose={handleCloseModal} />
-        </Modal>
-      )}
-      <Footer />
-    </div>
-  );
   return (
     <div>
       <div className="app">
