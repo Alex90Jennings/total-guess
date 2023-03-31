86f91a9adbdf10bcdfa0068a9aa74af1dca1c03d
Alex Jennings <aj0312@my.bristol.ac.uk>
2023-03-31 12:40:07 +0200

    optimise error calc

--- src/game/Results.jsx
+++ src/game/Results.jsx
@@ -3,34 +3,13 @@ import '../styles/game.css';
 
 function Results({ guess }) {
     const correctPrice = 45.65
-    const [error, setError] = useState({
-        difference: undefined,
-        percentageError: undefined
-    })
+    const [error, setError] = useState({ difference: undefined, percentageError: undefined })
 
     useEffect(() => {
-        if(correctPrice > guess) {
-            const difference = correctPrice - guess
-            const percentage = (difference / correctPrice) * 100
-            setError({
-                difference: difference,
-                percentageError: percentage
-            })
-        }
-        if(correctPrice < guess) {
-            const difference = guess - correctPrice
-            const percentage = (difference / correctPrice) * 100
-            setError({
-                difference: difference,
-                percentageError: percentage
-            })
-        }
-        if(correctPrice === guess) {
-            setError({
-                difference: 0,
-                percentageError: 0
-            })
-        }
+        let difference
+        correctPrice >= guess ? difference = correctPrice - guess : difference = guess - correctPrice
+        const percentage = (difference / correctPrice) * 100
+        setError({ difference: difference, percentageError: percentage })
     }, [guess]);
     
     return (
@@ -49,7 +28,7 @@ function Results({ guess }) {
                 </div>
                 <div className="percentile-results">
                     <div className="percentile-box">RANK</div>
-                    <p>{error.percentageError?.toFixed(0)}5</p>
+                    <p>{error.percentageError?.toFixed(0)}%</p>
                 </div>
                 <div className="statistics-results">
                     <a className='statistics-button-styling' href="/statistics">Statistics</a>
