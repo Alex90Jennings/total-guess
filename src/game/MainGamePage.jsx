934fd9d4c051a5fa69be31afe6be93770fb8f9fd
Chris Hyde <c_hyde333@hotmail.com>
2023-04-15 19:24:19 +0100

    Changed styling

--- src/game/MainGamePage.jsx
+++ src/game/MainGamePage.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
-import '../styles/game.css'
+import '../styles/game.css';
 import Product from './Product';
 import Submit from './Submit';
 
 function MainGamePage({ setGuess }) {
     const [readyToSubmit, setReadyToSubmit] = useState(false);
-    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalPrice, setTotalPrice] = useState(0); // Add this line
 
     return (
         <main id="main">
             {
                 readyToSubmit ?
-                    <Submit setGuess={setGuess} correctPrice={totalPrice}/> :
-                    <Product setReadyToSubmit={setReadyToSubmit} setTotalPrice={setTotalPrice}/>
+                    <Submit setGuess={setGuess} correctPrice={totalPrice} /> :
+                    <Product setReadyToSubmit={setReadyToSubmit} setTotalPrice={setTotalPrice} /> 
             }
         </main>        
     );
