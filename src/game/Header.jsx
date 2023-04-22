d63ce7c8cac7d3fedda4ea2f7c6e69b7a049b873
Chris Hyde <c_hyde333@hotmail.com>
2023-04-22 11:52:24 +0100

    Changed modal colours & did instructions modal component

--- src/game/Header.jsx
+++ src/game/Header.jsx
@@ -1,48 +1,75 @@
 import React from "react";
+import Instructions from "../game/Instructions";
+import { useState } from "react";
+import Modal from "react-modal";
+import "../styles/modal.css";
 
 function Header() {
-    return (
-        <header id="header">
-        <div className="header-left">
-            <div className="signup">Sign up</div>
-            <div className="signin">Sign in</div>
-        </div>
-        <div className="header-middle">CANTAB</div>
-        <div className="header-right">
-            <div className="icon1">
+  const [showModal, setShowModal] = useState(false);
+
+  const handleImageClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <header id="header">
+      <div className="header-left">
+        <div className="signup">Sign up</div>
+        <div className="signin">Sign in</div>
+      </div>
+      <div className="header-middle">CANTAB</div>
+      <div className="header-right">
+        <div>
+          <div className="icon1">
             <div>
-                <img
+              <img
                 src={"/icons/iicon.svg"}
                 alt="i icon"
                 height="80px"
                 width="80px"
-                />
+                onClick={handleImageClick}
+              />
             </div>
-            </div>
-            <div className="icon2">
-            <div>
-                <img
-                src={"/icons/settingsicon.svg"}
-                alt="settings icon"
-                height="80px"
-                width="80px"
-                />
-            </div>
-            </div>
-            <div className="icon3">
-            <div>
-                <img
-                src={"/icons/statisticsicon.svg"}
-                alt="fav icon"
-                height="50px"
-                width="50px"
-                />
-            </div>
-            </div>
+          </div>
+          {showModal && (
+            <Modal
+              className={"modal"}
+              isOpen={showModal}
+              onRequestClose={handleCloseModal}
+            >
+              <Instructions onClose={handleCloseModal} />
+            </Modal>
+          )}
         </div>
-        </header>
-    );
+        <div className="icon2">
+          <div>
+            <img
+              src={"/icons/settingsicon.svg"}
+              alt="settings icon"
+              height="80px"
+              width="80px"
+            />
+          </div>
+        </div>
+        <div className="icon3">
+          <div>
+            <img
+              src={"/icons/statisticsicon.svg"}
+              alt="fav icon"
+              height="50px"
+              width="50px"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
+
 
