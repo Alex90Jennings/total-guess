71ac6dfa0688465c017f92aef3979a6d02d8baec
Alex Jennings <aj0312@my.bristol.ac.uk>
2023-06-03 15:30:25 +0200

    header now works with media queries

--- src/game/Header.jsx
+++ src/game/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useState } from "react";
 import GameInstructions from "./GameInstructions";
 import { useEffect } from "react";
