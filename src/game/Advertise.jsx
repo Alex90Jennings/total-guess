12465e3c6a90be02afadf276d64900289ee06825
Chris Hyde <c_hyde333@hotmail.com>
2023-06-23 16:35:34 +0100

    Advertise with us

--- src/game/Advertise.jsx
+++ src/game/Advertise.jsx
@@ -17,9 +17,8 @@ function Advertise({ onClose }) {
                 <p>We offer a fun and engaging experience, which would be ideal association for many retail brands.</p>
             </div>
             <div className='list-reset pl-m'>
-                <p>If you would like to learn more about opportunities to collaborate with Cantab, please let us know.</p>
+                <p>If you would like to learn more about opportunities to collaborate with Cantab, please let us know.  totalguessgame@gmail.com</p>
             </div>
-            <button className="contact-button">Contact us</button>
         </div>
     );
 }
