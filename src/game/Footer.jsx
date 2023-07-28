3813f510ed0c8ef9ea164b2f71df1a57193a2d20
Chris Hyde <c_hyde333@hotmail.com>
2023-07-28 17:44:16 +0100

    New header & footer styling

--- src/game/Footer.jsx
+++ src/game/Footer.jsx
@@ -1,52 +1,86 @@
 import React from 'react';
+import { useContext } from 'react';
+import '../styles/modal.css';
+import { AppContext } from '../hooks/context';
+import { ModalToDisplay } from '../App';
 
 function Footer() {
-    
+    const { setModalToDisplay } = useContext(AppContext);
+
     return (
         <footer className="footer--layout">
-            <div className="footer--top">
-                <div></div>
+            <div className="left-icons">
                 <div className="icon--inst">
                     <div>
-                        <img 
-                            src="/icons/insta.png" 
+                        <img
+                            src="/icons/tinstagram.png"
                             alt="instagram icon"
-                            height="35px" 
+                            height="35px"
                             width="35px"
                         />
                     </div>
                 </div>
                 <div className="icon--linked">
-                        <div>
-                        <img 
-                            src={'icons/linkedin.png'} 
+                    <div>
+                        <img
+                            src={'/icons/tlinkedin.png'}
                             alt="linkedin icon"
-                            height="35px" 
+                            height="35px"
                             width="35px"
                         />
                     </div>
                 </div>
                 <div className="icon--twitter">
                     <div>
-                        <img 
-                            src={'icons/twitter.png'} 
+                        <img
+                            src={'/icons/ttwitter.png'}
                             alt="twitter icon"
-                            height="35px" 
+                            height="35px"
                             width="35px"
                         />
                     </div>
                 </div>
                 <div className="icon--facebook">
                     <div>
-                        <img 
-                            src={'icons/facebook.png'} 
+                        <img
+                            src={'/icons/tfacebook.png'}
                             alt="facebook icon"
-                            height="35px" 
+                            height="35px"
                             width="35px"
                         />
                     </div>
                 </div>
-                <div></div>
+            </div>
+            <div className="middle-content">
+                <div className="icon-mail">
+                    <img
+                        src={'/icons/mail.png'}
+                        alt="icon"
+                        height="35px"
+                        width="35px"
+                    />
+                </div>
+                <div className="contact">totalguessgame@gmail.com</div>
+            </div>
+            <div className="right-content">
+                <div
+                    className="footer-row"
+                    onClick={() => setModalToDisplay(ModalToDisplay.ABOUT_US)}
+                >
+                    About Us
+                </div>
+                <div
+                    className="footer-row"
+                    onClick={() => setModalToDisplay(ModalToDisplay.FAQ)}
+                >
+                    FAQ
+                </div>
+                <div
+                    className="footer-row"
+                    onClick={() => setModalToDisplay(ModalToDisplay.ADVERTISE)}
+                >
+                    Advertise with us
+                </div>
             </div>
         </footer>
     );
--- src/game/Header.jsx
+++ src/game/Header.jsx
@@ -1,30 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useContext, useState } from "react";
-import "../styles/modal.css";
-import "../styles/landingPage.css";
-import "../styles/game.css";
-import { AppContext } from "../hooks/context";
-import { ModalToDisplay } from "../App";
+import React, { useContext, useState } from 'react';
+import '../styles/modal.css';
+import '../styles/landingPage.css';
+import '../styles/game.css';
+import { AppContext } from '../hooks/context';
+import { ModalToDisplay } from '../App';
 import { useNavigate } from 'react-router-dom';
 
 function Header({ setShowLoginPage }) {
-
     const { loggedInUser, isAuthenticated, setModalToDisplay, handleSignOut, isMuted, setIsMuted } = useContext(AppContext);
     const navigate = useNavigate();
-    const [audio] = useState(new Audio("/Sounds/click.wav"));
+    const [audio] = useState(new Audio('/Sounds/click.wav'));
     const [showDropdown, setShowDropdown] = useState(false);
 
     //TODO: this won't work unless the current page is /
     const handleSignInClick = () => {
-        if(!isMuted) audio.play()
+        if (!isMuted) audio.play();
         setShowLoginPage(true);
     };
 
     const getInitials = () => {
         if (loggedInUser?.firstName && loggedInUser?.lastName) return `${loggedInUser?.firstName.charAt(0)}${loggedInUser?.lastName.charAt(0)}`;
-        return "??";
+        return '??';
     };
-
 
     return (
         <header id="header">
@@ -35,7 +33,7 @@ function Header({ setShowLoginPage }) {
                     className="icon-mute"
                     onClick={() => navigate('/')}
                 />
-                <div className={loggedInUser?.firstName ? "user-initials ml-xs mr-s" : "user-initials hide-text ml-xs mr-s"}>
+                <div className={loggedInUser?.firstName ? 'user-initials ml-xs mr-s' : 'user-initials hide-text ml-xs mr-s'}>
                     {getInitials()}
                 </div>
             </div>
@@ -46,14 +44,14 @@ function Header({ setShowLoginPage }) {
                     className="dropdown-icon"
                     onClick={() => navigate('/')}
                 />
-                <div className={loggedInUser?.firstName ? "user-initials ml-xs" : "user-initials hide-text ml-xs"}>
+                <div className={loggedInUser?.firstName ? 'user-initials ml-xs' : 'user-initials hide-text ml-xs'}>
                     {getInitials()}
                 </div>
             </div>
             <div className="header-middle logo">Total GuEss</div>
             <div className="header-right header-right-wide-screen">
-                <img 
-                    src={isMuted ? "/icons/tmute.png" : "/icons/tvolume.png"}
+                <img
+                    src={isMuted ? '/icons/tmute.png' : '/icons/tvolume.png'}
                     alt="mute"
                     className="icon-mute"
                     onClick={() => setIsMuted(!isMuted)}
@@ -74,9 +72,9 @@ function Header({ setShowLoginPage }) {
             <div className="header-right-narrow-screen">
                 <div className="two-columns-expand-one">
                     <img
-                        src={isMuted ? "/icons/muted.png" : "/icons/unmuted.png"}
+                        src={isMuted ? '/icons/muted.png' : '/icons/unmuted.png'}
                         alt="mute"
-                        className={isMuted ? "mr-m dropdown-icon pd-s" : "mr-m dropdown-icon"}
+                        className={isMuted ? 'mr-m dropdown-icon pd-s' : 'mr-m dropdown-icon'}
                         onClick={() => setIsMuted(!isMuted)}
                     />
                     <img
@@ -86,63 +84,59 @@ function Header({ setShowLoginPage }) {
                         onClick={() => setShowDropdown(!showDropdown)}
                     />
                 </div>
-                {
-                    showDropdown && (
-                        <div className="dropdown-menu">
-                            {
-                                !isAuthenticated ? (
-                                    <div className="dropdown-item" onClick={() =>  { 
-                                            handleSignInClick(); 
-                                            setShowDropdown(!showDropdown) 
-                                        }}>
-                                        Sign in
-                                    </div>
-                                ) : (
-                                    <div className="dropdown-item" onClick={() => { 
-                                            handleSignOut() 
-                                            navigate('/')
-                                            setShowDropdown(!showDropdown)
-                                        }}>
-                                        Sign out
-                                    </div>
-                                )
-                            }     
-                                <div className="dropdown-item" onClick={() => { 
-                                        setModalToDisplay(ModalToDisplay.STATISTICS); 
-                                        setShowDropdown(!showDropdown) 
-                                    }}>
-                                    Statistics
-                                </div>
-                                <div className="dropdown-item" onClick={() => { 
-                                        setModalToDisplay(ModalToDisplay.INSTRUCTIONS); 
-                                        setShowDropdown(!showDropdown) 
-                                    }}>
-                                    Instructions
-                                </div>
-                                <div className="dropdown-item" onClick={() => { 
-                                        setModalToDisplay(ModalToDisplay.ABOUT_US); 
-                                        setShowDropdown(!showDropdown) 
-                                    }}>
-                                    About Us
-                                </div>
-                                <div className="dropdown-item" onClick={() => { 
-                                        setModalToDisplay(ModalToDisplay.FAQ); 
-                                        setShowDropdown(!showDropdown) 
-                                    }}>
-                                    FAQs
-                                </div>
-                                <div className="dropdown-item" onClick={() => { 
-                                        setModalToDisplay(ModalToDisplay.ADVERTISE); 
-                                        setShowDropdown(!showDropdown) 
-                                    }}>
-                                    Advertise With Us
-                                </div>
-                                <div className="dropdown-item">
-                                    totalguessgame@gmail.com
-                                </div>
+                {showDropdown && (
+                    <div className="dropdown-menu">
+                        {!isAuthenticated ? (
+                            <div className="dropdown-item" onClick={() => {
+                                handleSignInClick();
+                                setShowDropdown(!showDropdown);
+                            }}>
+                                Sign in
+                            </div>
+                        ) : (
+                            <div className="dropdown-item" onClick={() => {
+                                handleSignOut();
+                                navigate('/');
+                                setShowDropdown(!showDropdown);
+                            }}>
+                                Sign out
+                            </div>
+                        )}
+                        <div className="dropdown-item" onClick={() => {
+                            setModalToDisplay(ModalToDisplay.STATISTICS);
+                            setShowDropdown(!showDropdown);
+                        }}>
+                            Statistics
                         </div>
-                    )
-                }
+                        <div className="dropdown-item" onClick={() => {
+                            setModalToDisplay(ModalToDisplay.INSTRUCTIONS);
+                            setShowDropdown(!showDropdown);
+                        }}>
+                            Instructions
+                        </div>
+                        <div className="dropdown-item" onClick={() => {
+                            setModalToDisplay(ModalToDisplay.ABOUT_US);
+                            setShowDropdown(!showDropdown);
+                        }}>
+                            About Us
+                        </div>
+                        <div className="dropdown-item" onClick={() => {
+                            setModalToDisplay(ModalToDisplay.FAQ);
+                            setShowDropdown(!showDropdown);
+                        }}>
+                            FAQs
+                        </div>
+                        <div className="dropdown-item" onClick={() => {
+                            setModalToDisplay(ModalToDisplay.ADVERTISE);
+                            setShowDropdown(!showDropdown);
+                        }}>
+                            Advertise With Us
+                        </div>
+                        <div className="dropdown-item">
+                            totalguessgame@gmail.com
+                        </div>
+                    </div>
+                )}
             </div>
         </header>
     );
